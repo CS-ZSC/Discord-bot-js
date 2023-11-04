7d9d1b20cdae62a80968456f1669108ad8580054
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-11-04 20:33:55 +0200

    return

--- main.js
+++ main.js
@@ -1,32 +1,31 @@
 const Discord = require("discord.js");
 const Levels = require("discord-xp");
 const fs = require("fs");
-
-require('dotenv').config({path: './.env'});
+require('dotenv').config({ path: './.env' });
 //require("dotenv").config();
 const client = new Discord.Client({
-    intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"],
+  intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"],
 });
 
 module.exports.client = client; //For Helper Functions like Announce
 for (const eventFile of fs
-    .readdirSync("./events")
-    .filter((file) => file.endsWith(".js"))) {
-    let event = require(`./events/${eventFile}`);
-    if (event.once) client.once(event.name, (...args) => event.execute(...args));
-    else client.on(event.name, (...args) => event.execute(...args));
+  .readdirSync("./events")
+  .filter((file) => file.endsWith(".js"))) {
+  let event = require(`./events/${eventFile}`);
+  if (event.once) client.once(event.name, (...args) => event.execute(...args));
+  else client.on(event.name, (...args) => event.execute(...args));
 }
 
 const http = require('http');
-const requestListener = async function (req, res) {
-    const delay =ms => new Promise(res => setTimeout(res, ms));
-    delay(3000000).then(r => res.end('Hello, World!'));
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello, World!');
 }
 const server = http.createServer(requestListener);
 server.listen(process.env.PORT || 5000);
 
 process.on('uncaughtException', function (err) {
-    console.log('Caught exception: ', err);
+  console.log('Caught exception: ', err);
 });
 
 

e7033e0db1fd70fb7ae9736701db749e11bcdfbd
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-11-23 16:08:51 +0200

    Added encrypt script which run the encrypt.js file

--- helpers/sheets/secure-file.js
+++ helpers/sheets/secure-file.js
@@ -1,53 +1,60 @@
 const crypto = require('crypto');
+const path = require("path");
 const fs = require('fs').promises;
 const algorithm = 'aes-256-gcm';
+require("dotenv").config();
 
-const SECRET_SALT = process.env.SECRET_SALT
-const SECRET_PASSPHRASE = process.env.SECRET_PASSPHRASE
+const SECRET_SALT = process.env.SECRET_SALT;
+const SECRET_PASSPHRASE = process.env.SECRET_PASSPHRASE;
+
+if (!SECRET_SALT || !SECRET_PASSPHRASE) {
+    console.error("Please set the SECRET_SALT and SECRET_PASSPHRASE environment variables.");
+    process.exit(1);
+}
 
 const secretKey = crypto.scryptSync(SECRET_PASSPHRASE, SECRET_SALT, 32);
 
-async function encryptFile(inputPath, key=secretKey) {
-  const iv = crypto.randomBytes(16);
-  const cipher = crypto.createCipheriv(algorithm, key, iv);
+async function encryptFile(inputPath, key = secretKey) {
+    const iv = crypto.randomBytes(16);
+    const cipher = crypto.createCipheriv(algorithm, key, iv);
 
-  const input = await fs.readFile(inputPath, 'utf8');
+    const input = await fs.readFile(inputPath, 'utf8');
 
-  const encrypted = Buffer.concat([cipher.update(input, 'utf8'), cipher.final()]);
-  const authTag = cipher.getAuthTag();
+    const encrypted = Buffer.concat([cipher.update(input, 'utf8'), cipher.final()]);
+    const authTag = cipher.getAuthTag();
 
-  const result = {
-    iv: iv.toString('hex'),
-    auth_tag: authTag.toString('hex'),
-    data: encrypted.toString('base64'),
-  };
+    const result = {
+        iv: iv.toString('hex'),
+        auth_tag: authTag.toString('hex'),
+        data: encrypted.toString('base64'),
+    };
+    await fs.writeFile(`./helpers/sheets/creds.json.secure`, JSON.stringify(result));
 
-  await fs.writeFile(`${inputPath}.secure`, JSON.stringify(result));
-
-  console.log(`Encrypted file written to ${inputPath}.secure!`);
+    console.log(`Encrypted file written to ${inputPath}.secure!`);
 }
 
-async function decryptToString(inputPath, key=secretKey) {
-  console.log(`Decrypting ${inputPath}...`);
-  const encryptedData = JSON.parse(await fs.readFile(inputPath, 'utf8'));
+async function decryptToString(inputPath, key = secretKey) {
 
-  const decipher = crypto.createDecipheriv(
-    algorithm,
-    key,
-    Buffer.from(encryptedData.iv, 'hex'),
-  );
+    console.log(`Decrypting ${inputPath}...`);
+    const encryptedData = JSON.parse(await fs.readFile(inputPath, 'utf8'));
 
-  decipher.setAuthTag(Buffer.from(encryptedData.auth_tag, 'hex'));
+    const decipher = crypto.createDecipheriv(
+        algorithm,
+        key,
+        Buffer.from(encryptedData.iv, 'hex'),
+    );
 
-  const decrypted = Buffer.concat([
-    decipher.update(Buffer.from(encryptedData.data, 'base64')),
-    decipher.final(),
-  ]);
-  return decrypted.toString('utf8');
+    decipher.setAuthTag(Buffer.from(encryptedData.auth_tag, 'hex'));
+
+    const decrypted = Buffer.concat([
+        decipher.update(Buffer.from(encryptedData.data, 'base64')),
+        decipher.final(),
+    ]);
+    return decrypted.toString('utf8');
 }
 
 
 module.exports = {
-  encryptFile,
-  decryptToString
+    encryptFile,
+    decryptToString
 }

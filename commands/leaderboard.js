2c7ebcee886fcf4e28fab5a8e2384483ba9ab015
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-11-24 13:14:41 +0200

    Made it so that command can be called from schedule command

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -2,16 +2,19 @@ const { getLeaderboard } = require("../helpers/getLeaderboard");
 const card = require("../helpers/levelCard");
 const leaderboard = require("../helpers/levelCard");
 const { client } = require("../main");
+const config = require('../config.json');
 
 module.exports = {
   name: "leaderboard",
   description: "Get the highest 10 members",
   slash: true,
   category: "Leaderboard",
-  callback: async ({ interaction }) => {
-    interaction.reply({
-      content: "Working on it",
-    });
+  callback: async (interaction = null) => {
+    if (interaction) {
+      interaction.reply({
+        content: "Working on it",
+      });
+    }
     const leaderboard = await getLeaderboard();
     for (let rank = 0; rank < 10; ++rank) {
       if (leaderboard.length <= rank) {
@@ -20,13 +23,22 @@ module.exports = {
       const user = await client.users.fetch(leaderboard[rank].userID);
       const RankCard = await card(user, rank + 1);
       RankCard.build({ fontX: "Arial", fontY: "Arial" }).then((buffer) => {
-        interaction.followUp({
-          files: [{ attachment: buffer }],
-        });
+        if (interaction){
+          interaction.followUp({
+            files: [{ attachment: buffer }],
+          });
+        }else {
+          const channel = client.channels.cache.get(config.serverInfo.leaderboard_id);
+          channel.send({
+            files: [{ attachment: buffer }],
+          });
+        }
       });
     }
-    interaction.editReply({
-      content: `Here is the leaderboard`,
-    });
+    if (interaction){
+      interaction.editReply({
+        content: `Here is the leaderboard`,
+      });
+    }
   },
 };

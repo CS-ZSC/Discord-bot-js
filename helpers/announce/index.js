9bbf4a840b05a404403d0a2d71bf047938f9e662
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-11-24 16:08:53 +0200

    Fixed some typos

--- helpers/announce/index.js
+++ helpers/announce/index.js
@@ -4,6 +4,6 @@ const client = require("../../main").client;
 /* TODO: FIX this*/
 module.exports = {
   async announce(message) {
-    client.channels.cache.get(config.serverInfo.announcments_id).send(message);
+    client.channels.cache.get(config.serverInfo.announcements_id).send(message);
   }
 }

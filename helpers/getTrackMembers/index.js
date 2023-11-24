fa857c693fdbcbfeeae51cc99f83390e15b910a5
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-11-24 13:12:21 +0200

    Refactored the get member function, so it only takes the track as a parameter

--- helpers/getTrackMembers/index.js
+++ helpers/getTrackMembers/index.js
@@ -1,16 +1,28 @@
 /*
-  Get track memebers
+  Get track members
 */
 const client = require("../../main").client;
 const config = require("../../config.json");
 
 module.exports = {
-  async getMembers(interaction, track) {
+  async getMembers(track) {
     const trackId = config.roles[track];
-    await interaction.guild.members.fetch();
-    return client.guilds.cache
-        .get(config.serverInfo.GUILD_ID)
-        .roles.cache.get(trackId)
-        .members.map((m) => m.user);
+    const guild = client.guilds.cache.get(config.serverInfo.GUILD_ID);
+
+    if (!guild) {
+      console.error(`Guild not found with ID: ${config.serverInfo.GUILD_ID}`);
+      return [];
+    }
+
+    await guild.members.fetch();
+
+    const trackRole = guild.roles.cache.get(trackId);
+
+    if (!trackRole) {
+      console.error(`Role not found with ID: ${trackId}`);
+      return [];
+    }
+
+    return trackRole.members.map((m) => m.user);
   },
 };

4491b14a66c36a94ff58fd4cfdb9455e9f0e7e35
hulxv <hulxxv@gmail.com>
2024-12-06 13:58:00 +0200

    feat: improve logging command

---
+++ events/interacationCreate.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: "interactionCreate",
+    once: true,
+    async execute(interaction) {
+        console.log(interaction);
+        if (interaction.isCommand()) {
+            console.debug(`
+ DEBUG: Command executed 
+   by: ${interaction.user.globalName} (${interaction.user.tag})
+   user_id: (${interaction.user.id})
+   command: ${interaction.commandName}
+            `);
+        }
+
+    }
+}
+
